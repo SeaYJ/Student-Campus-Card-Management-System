eeb508c22e027b89f160dd94cc35b1d3497ebc54
SeaYJ <seayj21@outlook.com>
2024-02-07 22:02:58 +0800

    @@ 完善 DISALLOW_COPY_AND_ASSIGN() 的定义 @@

--- src/SPECIAL_DEFINITION_MACRO.h
+++ src/SPECIAL_DEFINITION_MACRO.h
@@ -2,9 +2,11 @@
 #define SPECIAL_DEFINITION_MACRO_H_
 
 // 禁止类的默认拷贝构造函数
+#ifndef DISALLOW_COPY_AND_ASSIGN
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
     TypeName(const TypeName&) = delete; \
     void operator=(const TypeName&) = delete;
+#endif
 
 
 #endif // !SPECIAL_DEFINITION_MACRO_H_
